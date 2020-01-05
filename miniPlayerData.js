e60189105105f48d97d0f6f88e5fa830f85709ba
artempecunia2019 <artempecunia2019@gmail.com>
2020-01-05 03:12:03 +0300

    v1
    deneme

---
+++ miniPlayerData.js
@@ -0,0 +1,15 @@
+klynt.miniPlayerData = {
+    "thanksForWatchingWording": "Thanks for watching!",
+    "resumePlaybackWording": "Resume playback?",
+    "fullscreenInfoWording": "This program will launch in fullscreen",
+    "yesWording": "Yes",
+    "title": "artem_pecunia",
+    "thumbnail": "Medias/Photos/home_screen-2.jpg",
+    "noWording": "No",
+    "analyticsKey": "",
+    "downloadAppWording": "Download App",
+    "redirectToMobileApp": "auto",
+    "url": "",
+    "launchAppWording": "Then Launch Project",
+    "description": "subtitle"
+}
